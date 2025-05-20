e1073632bedad636eb55c379a15365b85439f3d1
ramazon07-cmd <ustau2453@gmail.com>
2025-05-20 23:36:42 +0500

    Configure static and media files for Vercel deployment

--- core/settings.py
+++ core/settings.py
@@ -131,7 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
