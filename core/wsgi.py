6932d620fefd3646fe4e6a96faa7cf0d63c6baa3
ramazon07-cmd <ustau2453@gmail.com>
2025-05-21 15:45:52 +0500

    h

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for config project.
+WSGI config for core project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-app = get_wsgi_application()
+application = get_wsgi_application()
