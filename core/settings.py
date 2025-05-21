fd6eff93af34e8c6650e8f4dd8985d6a5a9e2e10
ramazon07-cmd <ustau2453@gmail.com>
2025-05-21 16:12:57 +0500

    fehim

--- core/settings.py
+++ core/settings.py
@@ -128,8 +128,8 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'staticfiles/media/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
