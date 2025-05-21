a3a1034690e5755e030482924a54ab875a9d4437
ramazon07-cmd <ustau2453@gmail.com>
2025-05-21 15:35:18 +0500

    kk

--- core/settings.py
+++ core/settings.py
@@ -132,7 +132,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
