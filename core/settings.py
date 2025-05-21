8ba067d6cf5b4b72d619944da8c59bf2ecc9692a
ramazon07-cmd <ustau2453@gmail.com>
2025-05-21 07:43:54 +0500

    ff

--- core/settings.py
+++ core/settings.py
@@ -125,11 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 # Static fayllar
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Agar `static/` papka bo‘lsa
-
-MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # oldingi staticfiles emas
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
